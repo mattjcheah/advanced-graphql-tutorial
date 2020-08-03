5f88d2651d2aa2952b5bb445c5001acc664ddc32
Matt Cheah <matt.j.cheah@gmail.com>
2020-08-03 14:25:59 +1000

    Ch1: Install and Configure Apollo Federation and Apollo Gateway

--- server/src/config/apollo.js
+++ server/src/config/apollo.js
@@ -1,3 +1,13 @@
+import { ApolloGateway } from "@apollo/gateway";
 import { ApolloServer } from "apollo-server-express";
+
+const gateway = new ApolloGateway({
+  serviceList: [{ name: "accounts", url: process.env.ACCOUNTS_SERVICE_URL }],
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
 
 export default server;
